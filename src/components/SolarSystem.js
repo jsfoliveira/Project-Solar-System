55626a84c7f8199f4a4a6fce7f57c2a98959208d
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-02-18 15:48:02 -0300

    Requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
         <h1>Sistema Solar</h1>
+        <Title headline="Planetas" />
       </header>
     );
   }
