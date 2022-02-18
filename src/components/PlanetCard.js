1cfab7c42f8c4241feaee632c56ee2df36d8fde2
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-02-18 15:56:58 -0300

    Consertei erro de validaçao do title

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Olá</h1>
+        <h2>oi</h2>
+      </>
+    );
+  }
+}
+
+export default PlanetCard;
