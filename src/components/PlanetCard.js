559d921777e49115afdd6c8c335f8da69db40741
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-02-18 16:36:46 -0300

    Requisito 5 feito. Comecei a fazer o 6

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
+    const { planetImage, planetName } = this.props;
     return (
-      <>
-        <h1>Olá</h1>
-        <h2>oi</h2>
-      </>
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          { planetName }
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
     );
   }
 }
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Title from './Title';
 
-class Header extends React.Component {
+class SolarSystem extends React.Component {
   render() {
     return (
       <header>
-        <h1>Sistema Solar</h1>
+        <h1>S</h1>
         <Title headline="Planetas" />
       </header>
     );
   }
 }
 
-export default Header;
+export default SolarSystem;
