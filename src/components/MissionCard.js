08b38b4ecab1c7f95e3a39267fd325b969eaab2c
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-03-02 20:48:01 -0300

    alteraçao desnecessaria

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 // O MissionCard tem uma div que vai receber o atributo data-testid="mission-card". Vai receber também as props: name, year, country, destination. Essas props serão renderizados na tag <p>. Para usar as props, precisa validá-las.
 class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
+      <div data-testid="mission-card" className="mission-card">
         <p data-testid="mission-name">
           { name }
         </p>
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,22 +2,25 @@ import React from 'react';
 import Title from './Title';
 import missoes from '../data/missions';
 import MissionCard from './MissionCard';
+import '../App.css';
 
 // Missions vai renderizar o Title e o MissionCard (esse que deve renderizar o name, year, country, destination).
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <>
         <Title headline="Missões" />
-        {missoes
-          .map((missao) => (<MissionCard
-            name={ missao.name }
-            year={ missao.year }
-            destination={ missao.destination }
-            country={ missao.country }
-            key={ missao.name }
-          />))}
-      </div>
+        <div data-testid="missions" className="container-missions">
+          {missoes
+            .map((missao) => (<MissionCard
+              name={ missao.name }
+              year={ missao.year }
+              destination={ missao.destination }
+              country={ missao.country }
+              key={ missao.name }
+            />))}
+        </div>
+      </>
     );
   }
 }
